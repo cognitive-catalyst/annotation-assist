c7cf3e38aea225185bcd38c6ed2672337426eb4a
amblock <amblock@us.ibm.com>
2016-05-25 14:02:24 -0400

    built

--- static/build/bundle.js
+++ static/build/bundle.js
@@ -59874,11 +59874,12 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	var SimilarQuestions = function SimilarQuestions() {
+	var SimilarQuestions = function SimilarQuestions(_ref) {
+	    var questions = _ref.questions;
 	    return _react2.default.createElement(
 	        'ol',
 	        { className: 'other-question-container' },
-	        undefined.props.questions.map(function (item, i) {
+	        questions.map(function (item, i) {
 	            return _react2.default.createElement(
 	                'li',
 	                { key: i, className: 'other-question' },
